b4ef349d7e434fd1ef2d0d32b9945f59a6d1581d
Harshwardhan Dattatray Gaikwad <145912894+Harshwarsdhang7589@users.noreply.github.com>
2026-01-02 00:14:11 +0530

    Q2 STATIC METHOD AND CLASS

---
+++ Library.java
@@ -0,0 +1,20 @@
+/*
+
+    - Create a class "Library" having members:
+	    - static int totalBooks: which tracks the number of books. 
+	    - static int addBooks(int count): to add books in library.
+	    - int getBook(): to get the book from library.
+	    
+	- Ensure that whenever getBook method implemets it decreases the totalBook count.
+
+    - Create another class "Test" which test the functions to add and get the books and display the counts of books at the end. 
+
+
+*/
+package Q2;
+public class Library
+{
+	// non-static data member:
+	private String Bookname;
+	
+}
