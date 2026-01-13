dddfa4d254baac94f5081920c409e19d6e3b4bb2
Harshwardhan Dattatray Gaikwad <145912894+Harshwarsdhang7589@users.noreply.github.com>
2026-01-13 23:55:05 +0530

    intro

---
+++ Sumdemo.java
@@ -0,0 +1,25 @@
+package Q2;
+
+public class Sumdemo 
+{
+    public static void main(String[] args)
+    {
+        Xyz.Sum()
+        {
+            System.out.println("Cipher");
+
+        }
+    } class Xyz
+    {
+        staic void sum{
+            int a=2;
+            int b=9;
+            double c= (double) a+b;
+            System.out.println("Summation:"+c);
+
+
+        }
+
+    }
+    
+}
