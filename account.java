eb0a3bb4d332e19137cd34cca08a8e54e85a7688
Harshwardhan Dattatray Gaikwad <145912894+Harshwarsdhang7589@users.noreply.github.com>
2026-01-21 00:53:21 +0530

    Collection example1 java

---
+++ account.java
@@ -0,0 +1,14 @@
+public class account {
+    private int acid;
+    private String name;
+    private double salary;
+    
+    public account() {
+        System.out.println("The value is:");
+
+    }
+        
+    }
+
+    
+}
