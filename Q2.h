d1011e094d00e47170310087957f5bb666aaf2d3
Harshwardhan Dattatray Gaikwad <145912894+Harshwarsdhang7589@users.noreply.github.com>
2026-01-21 22:51:52 +0530

    CORE JAVA TEST ALL QUESTIONS STACK
    JAVA TEST CORE JAVA  FINAL TEST ALL QUESTIONS STACK

---
+++ Q2.h
@@ -0,0 +1,66 @@
+/*
+        CONCEPT: Polymorphism
+        
+    Q3] Create a program to demonstrate Runtime Polymorphism.Create a class Bank with a method getRateOfInterest().
+
+            Create subclasses SBI, HDFC, and ICICI.
+            -->Override getRateOfInterest() in each subclass.
+            --->Use parent reference to call child class methods.
+            
+    
+        CONCEPT:Abstraction         [ 4 MARKS ]
+
+    Q4] Create a program to demonstrate Abstraction.Create an ABSTRACT CLASS Shape with abstract methods calculateArea() and calculate Perimeter().
+
+    Create subclasses Circle and Rectangle.
+        ->Implement all abstract methods.
+        -->Use parent reference to call subclass methods.
+
+
+        COMMON OOPS BASED-PROJECT:
+
+    Q5] Common OOP Concepts                            [ 4 Marks ] 
+   
+    Design a Payment System that demonstrates all OOP concepts.Create an abstract class Payment.
+        -->Use private variables with getters and setters.
+        --->Create subclasses CreditCard Payment and UPIPayment.
+        ---->Override the pay() method.
+        
+        Use parent reference to invoke subclass methods.
+
+
+    CONCEPTS: EXCEPTION HANDLING
+
+    Q6] Exception Handling              [ 6 Marks ]
+
+    Create a user-defined exception class named EmployeeException in the package com.cipher.exception.
+        Throw the exception if the salary of an employee is below 3000.
+        Handle the exception properly using try-catch.
+
+        Display a meaningful error message.
+
+    CONCEPTS: STRING HANDLING
+
+Q7] String Handling                  [ 5 Marks ] 
+
+        Create a method to generate the mirror image of a string.
+        Method Name getimage
+        Argument String
+
+ PROGRAM :
+   Return Type String
+
+Example:
+
+Input EARTH
+
+Output EARTH|HTRAE
+
+Use StringBuffer or StringBuilder.
+
+Use the reverse() method.
+
+Join both strings using the pipe symbol.
+
+
+*/
---
+++ Q3.h
@@ -0,0 +1,25 @@
+/*
+  CONCEPT: COLLECTION FRAMEWORK                   [8 MARKS ] 
+
+Q8] Create a method which accepts a HashMap and returns the values of the map in sorted order as a List.
+
+Method Name: getValues
+            Argument HashMap
+            
+            Return Type List
+
+    The method should extract values from the HashMap, sort them, and return the sorted List.
+
+ 
+    CONCEPT: MULTITHREADING                       [ 7 MARKS ]
+
+Q9] Design a Railway Ticket Booking System using Multithreading.Create a class TicketCounter.Create multiple threads representing users.
+     ->Only one user should be allowed to book a ticket at a time.
+      -->Use synchronization.
+
+      If tickets are over, display the message No tickets available.
+      
+      
+      
+      
+      */
